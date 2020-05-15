3b7b7b96a3cf8ca7e595db1553d840ab87a951cc
iamolivermn <mitchelloliver@gmail.com>
2020-05-15 16:18:22 -0700

    added the functon render and save, you can add a task and is now svaed in the lcoal stroage

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-const listContainer = document.querySelector("[data-lists]")
-const newTaskForm = document.querySelector('[data-new-task-form]')
-const newTaskInput = document.querySelector('[data-new-task-input]')
+const taskContainer = document.querySelector('[data-tasks]')
+const newTaskForm = document.querySelector("[data-new-task-form]")
+const newTaskInput = document.querySelector("[data-new-task-input]")
 
-let lists = ["name" , "todo"]
 
-newTaskForm.addEventListener("submit" , e =>{
+const LOCAL_STORAGE_TASK_KEY = "task.lists"
+let tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY))||[]
+
+newTaskForm.addEventListener("submit", e =>{
     e.preventDefault()
-    const taskName = newTaskInput.value
-    if (taskName == null || taskName === "") return 
+    const taskName = newTaskInput.value 
+    if (taskName == null || taskName ==="") return 
     const task = createTask(taskName)
-    newTaskInput.value = null 
+    newTaskInput.value = null
     tasks.push(task)
-    render()
+   saveAndRender()
 })
 
 function createTask(name){
-    return{id: Date.now().toString(), name: name }
-
+    return{ id: Date.now().toString(), name: name}
 }
 
+function saveAndRender(){
+    save()
+    render()
+}
 
+function save(){
+    localStorage.setItem(LOCAL_STORAGE_TASK_KEY, JSON.stringify(tasks))
+}
 
-function render(){
-        
-    clearElement(listContainer)
-    lists.forEach(list => {
-        const listElement = document.createElement("li")
-        listElement.classList.add("task")
-        listElement.innerText =list
-        listContainer.appendChild(listElement)
-    })
+function render() {
+    clearElement(taskContainer)
+    tasks.forEach(task => {
+    const taskElement = document.createElement("li")
+    taskElement.dataset.taskId = task.id 
+    taskElement.classList.add("task")
+    taskElement.innerText = task.name
+    taskContainer.appendChild(taskElement)
+})
+
 }
 
 function clearElement(element){
-    while(element.firstChild) {
+    while(element.firstChild){
         element.removeChild(element.firstChild)
     }
 
 }
 
-render ()
+render()
