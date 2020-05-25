7412bceb753d775f799f1f06ea36fb17dc385a8b
iamolivermn <mitchelloliver@gmail.com>
2020-05-24 22:39:58 -0700

    added the date function to display the current date added more CSS mainproduct is done

--- script.js
+++ script.js
@@ -84,6 +84,25 @@ function clearElement(element){
 
 }
 
+function renderTime(){
+    var myDate = new Date();
+    var year = myDate.getYear();
+        if( year < 1000){
+            year += 1900;
+        }
+    var day = myDate.getDay();
+    var month = myDate.getMonth();
+    var dayM = myDate.getDate();
+    var dayArray = new Array("Sunday", "Monday", "Tuesday" , "Wednesday", "Thursday" , "Friday", "Saturday");
+    var monthArray = new Array( "January", "February", "March", "April", "May", "June", "July" , "August", "September", "October", "November", "December");
+
+    currentDate.textContent= "" + dayArray[day]+ " " +dayM+ " " +monthArray[month] ;
+    currentDate.innerText = "" + dayArray[day]+ " " +dayM+ " " +monthArray[month] ;
+
+    setTimeout("renderTime()",1000);
+}
+
 render()
+renderTime()
   
 
